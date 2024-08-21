fa69fe6c9c4641326e92fc66a69399ffa0f0db6a
Dani-ops-cyber <reyna.chambi@ucb.edu.bo>
2024-08-20 20:05:06 -0400

    holi

---
+++ labo1.py
@@ -0,0 +1,17 @@
+
+def vol_num(numero):
+    numero_str = str(numero) #cadena
+    numero_invertido_str = numero_str[::-1]
+
+    numero_invertido = int(numero_invertido_str)
+    return numero_invertido
+
+entrada_usuario = input("Introduce un número: ")
+
+if entrada_usuario.isdigit():
+    numero = int(entrada_usuario)
+    resultado = vol_num(numero)
+    print(f'Primer Nro: {numero}')
+    print(f'Número invertido: {resultado}')
+
+
