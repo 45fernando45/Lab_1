966b0f523a3b23ed445fa81c2f7078c1fec83360
45fernando45 <fernando.calle@ucb.edu.bo>
2024-08-27 17:23:55 -0400

    Actualizacion_fer

--- labo1.py
+++ labo1.py
@@ -15,6 +15,22 @@ if entrada_usuario.isdigit():
     resultado = vol_num(numero)
     print(f'Primer Nro: {numero}')
     print(f'Número invertido: {resultado}')
+    
+def suma_numeros(numero1, numero2):
+    return numero1 + numero2
+
+entrada_usuario = input("Introduce un número: ")
+
+if entrada_usuario.isdigit():
+    numero = int(entrada_usuario)
+    resultado = vol_num(numero)
+    suma = suma_numeros(numero, resultado)  #ojito
+    
+    print(f'Primer Nro: {numero}')
+    print(f'Número invertido: {resultado}')
+    print(f'Suma de ambos números: {suma}')
+else:
+    print("Por favor, introduce un número válido.")
 
 
 
